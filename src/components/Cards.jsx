896548bcdb8d0b00d52b207b87313b9ce1a69bb5
Michael Snook <me@michaelsnook.com>
2021-05-01 04:30:12 +0530

    Fix card navigation

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,13 +11,13 @@ export const Cards = (props) => {
         {nodes.map((item, i) => (
           <div
             className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6"
-            key={`card_${item.data.slug}`}
+            key={`card_${item.data.Slug}`}
           >
             <Card
               {...item.data}
               navigation={{
                 current: i,
-                items: nodes.map((item) => `/${item.data.slug}`),
+                items: nodes.map((item) => `/${item.data.Slug}`),
               }}
             />
           </div>
