076864853a95bbb2ad25982d66a81e1ecaedd287
Aakash Aggarwal <aakashaggarwal.1993@gmail.com>
2021-04-30 21:24:04 +0530

    Something went wrong in commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,44 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, withPrefix } from "gatsby"
 import { Cards, Hero, SiteMetadata } from "../components"
 import { Layout } from "../layouts/Layout"
+import Helmet from "react-helmet"
 
 export default ({ data }) => {
+  var fund_cta
+  if (typeof window !== "undefined") {
+    if (window.location.search === "") {
+      fund_cta = (
+        <a
+          href="/?foriegn_fund=true"
+          className="bg-pink-600 shadow-sm rounded-md text-white px-3 py-1 mr-2 toggle hidden md:flex w-full md:w-auto px-4 py-2"
+        >
+          Donate in £ $ €
+        </a>
+      )
+    } else {
+      fund_cta = (
+        <a
+          href="/"
+          className="bg-pink-600 shadow-sm rounded-md text-white px-3 py-1 mr-2 toggle hidden md:flex w-full md:w-auto px-4 py-2"
+        >
+          View All Funds
+        </a>
+      )
+    }
+  }
+
   var category_cards = {}
+  var categories = {}
   data.items.nodes.forEach((item) => {
     if (
       typeof window === "undefined" ||
-      (window.location.search !== "" && item["data"]["Foreign_Funds"] === true) ||
+      (window.location.search !== "" &&
+        item["data"]["Foreign_Funds"] === true) ||
       window.location.search === ""
     ) {
+      categories[item["data"]["Category_Rank"]] = item["data"]["Category"]
+
       if (category_cards[item["data"]["Category"]]) {
         category_cards[item["data"]["Category"]].push(item)
       } else {
@@ -21,11 +49,44 @@ export default ({ data }) => {
   })
   return (
     <Layout>
+      <Helmet>
+        <script src={withPrefix("nav_links.js")} type="text/javascript" />
+      </Helmet>
       <SiteMetadata
         title="Mutual Aid India"
         description="A curated list of opportunities to donate for COVID relief."
         image={data.hero.url}
       />
+      <nav className="flex flex-wrap items-center justify-between p-5 bg-blue-200">
+        <img src="/icon.png" alt="Logo" width="60" />
+        <div className="flex md:hidden">
+          <button id="hamburger">
+            <img
+              className="toggle block"
+              src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png"
+              width="40"
+              height="40"
+            />
+            <img
+              className="toggle hidden"
+              src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
+              width="40"
+              height="40"
+            />
+          </button>
+        </div>
+        <div className="toggle hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none">
+          {Object.keys(categories).map((category_header, index) => (
+            <a
+              href={"#category_" + category_header}
+              className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none max-w-xs"
+            >
+              {categories[category_header]}
+            </a>
+          ))}
+        </div>
+        {fund_cta}
+      </nav>
 
       <Hero
         image={data.hero}
@@ -65,8 +126,11 @@ export default ({ data }) => {
         </div>
       </div>
 
-      {Object.keys(category_cards).map((category_header, index) => (
-        <div className="container overflow-hidden pt-6">
+      {Object.keys(categories).map((category_header, index) => (
+        <div
+          className="container overflow-hidden pt-6"
+          id={"category_" + category_header}
+        >
           <h4 className="text-blue-800 uppercase text-sm tracking-wide font-medium pb-px">
             Category
           </h4>
@@ -74,10 +138,10 @@ export default ({ data }) => {
             className="text-2xl lg:text-4xl font-bold leading-tight text-black"
             key={index}
           >
-            {category_header}
+            {categories[category_header]}
           </h2>
 
-          <Cards nodes={category_cards[category_header]} />
+          <Cards nodes={category_cards[categories[category_header]]} />
         </div>
       ))}
     </Layout>
@@ -102,6 +166,7 @@ export const query = graphql`
           Urgent
           Foreign_Funds
           Category
+          Category_Rank
         }
       }
     }
