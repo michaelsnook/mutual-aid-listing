e01da78445da8d8db27f594a0627a09459201d63
Michael Snook <me@michaelsnook.com>
2021-04-30 18:27:22 +0530

    Add gtag for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,31 @@ module.exports = {
         postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          // Google Analytics / GA / Google Ads / Adwords / AW / Marketing Platform 
+          // advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          "G-MK3YMNPNW0",
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        /* gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          // anonymize_ip: true,
+          cookie_expires: 0,
+        }, */
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["mutual-aid-listing.pages.dev/*"],
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
