02b2ce12caa3794dc8cbf32a00835ea9517522c8
Michael Snook <me@michaelsnook.com>
2021-05-03 17:59:58 +0530

    Switch summary for rupee amounts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -145,7 +145,8 @@ export const query = graphql`
           }
           Name
           Slug
-          Summary
+          Rupees_Goal
+          Rupees_Reached
           Urgent
           Foreign_Funds
           Category
