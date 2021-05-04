0e1acca721058bb83ddc12b9b62b92dff0b43e1f
Aakash Aggarwal <aakashaggarwal.1993@gmail.com>
2021-05-04 19:17:07 +0530

    Highlighted campaigns

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { graphql, Link } from "gatsby"
 import { Cards, Hero, SiteMetadata } from "../components"
 import { Layout } from "../layouts/Layout"
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from "@heroicons/react/outline"
 import { Switch } from "@headlessui/react"
 
 export default ({ data }) => {
@@ -12,8 +12,13 @@ export default ({ data }) => {
 
   var category_cards = {}
   var categories = {}
+  categories[0] = "Hightighted Campaigns"
+  category_cards["Hightighted Campaigns"] = []
   data.items.nodes.forEach((item) => {
     if (item["data"]["Foreign_Funds"] === true || isForeignDonor === false) {
+      if (item["data"]["Urgent"] === true) {
+        category_cards["Hightighted Campaigns"].push(item)
+      }
       const { Category, Category_Rank } = item.data
       categories[Category_Rank] = Category
       category_cards[Category] = category_cards[Category] || []
@@ -38,17 +43,32 @@ export default ({ data }) => {
 
       <nav className="shadow-lg z-40 p-5 sticky top-0 bg-white w-full">
         <div className="flex flex-row justify-between w-full">
-          <button onClick={() => setIsNavOpen(!isNavOpen)} className="focus:outline-none focus:ring focus:border-primary-300 rounded-sm">
-            <MenuIcon className={`block h-6 w-6 ${isNavOpen ? 'hidden' : 'block'}`} aria-hidden={isNavOpen ? true : false} />
-            <XIcon className={`block h-6 w-6  ${isNavOpen ? 'block' : 'hidden'}`} aria-hidden={isNavOpen ? false : true} />
+          <button
+            onClick={() => setIsNavOpen(!isNavOpen)}
+            className="focus:outline-none focus:ring focus:border-primary-300 rounded-sm"
+          >
+            <MenuIcon
+              className={`block h-6 w-6 ${isNavOpen ? "hidden" : "block"}`}
+              aria-hidden={isNavOpen ? true : false}
+            />
+            <XIcon
+              className={`block h-6 w-6  ${isNavOpen ? "block" : "hidden"}`}
+              aria-hidden={isNavOpen ? false : true}
+            />
           </button>
 
           <div>
-            <span className="inline-flex flex-shrink-0 relative h-6 mr-2 align-text-bottom text-primary-900">Donate from overseas</span>
+            <span className="inline-flex flex-shrink-0 relative h-6 mr-2 align-text-bottom text-primary-900">
+              Donate from overseas
+            </span>
             <Switch
               checked={isForeignDonor}
-              onChange={() => {setIsForeignDonor(!isForeignDonor)}}
-              className={`relative inline-flex items-center flex-shrink-0 ${isForeignDonor? 'bg-primary-700' : 'bg-gray-600'} w-11 h-6 rounded-full cursor-pointer focus:outline-none focus:shadow-outline transition-colors duration-200 ease-in-out`}
+              onChange={() => {
+                setIsForeignDonor(!isForeignDonor)
+              }}
+              className={`relative inline-flex items-center flex-shrink-0 ${
+                isForeignDonor ? "bg-primary-700" : "bg-gray-600"
+              } w-11 h-6 rounded-full cursor-pointer focus:outline-none focus:shadow-outline transition-colors duration-200 ease-in-out`}
             >
               <span
                 className={`${
@@ -57,10 +77,12 @@ export default ({ data }) => {
               />
             </Switch>
           </div>
-
         </div>
-        <div className={`flex-col toggle w-full text-center ${isNavOpen? 'flex' : 'hidden'}`}>
-
+        <div
+          className={`flex-col toggle w-full text-center ${
+            isNavOpen ? "flex" : "hidden"
+          }`}
+        >
           {Object.keys(categories).map((category_header) => (
             <a
               onClick={() => setIsNavOpen(false)}
@@ -80,44 +102,56 @@ export default ({ data }) => {
           </Link>
           <a
             href="https://docs.google.com/document/d/e/2PACX-1vSFpy5vYw2wtESs77spBb1nv3dpGj3Jhv1J3WxMpfURc_MVIgc556s1BqD9z3GO-HVqLQWhWAHxGIOs/pub"
-            className="block text-primary-900 px-3 py-3 mx-auto hover:bg-primary-200 rounded-md">
+            className="block text-primary-900 px-3 py-3 mx-auto hover:bg-primary-200 rounded-md"
+          >
             List of Completed Fundraisers
           </a>
         </div>
-
       </nav>
 
-      {!isAlertClosed && <div className="container pt-6">
-        <div className="relative flex flex-wrap bg-yellow-200 border border-yellow-400 rounded-md py-5 px-8 max-w-3xl mx-auto">
-          <button className="absolute top-0 right-0 p-3" onClick={() => {setIsAlertClosed(true)}} area-pressed="false">✕</button>
-          <p className="mt-2 mb-1 font-bold">Info for donors/supporters:</p>
-          <p className="mb-2">
-            <ul className="list-disc pl-5">
-              <li>
-                Please filter for urgent needs first, marked in red as "URGENT"
-              </li>
-              <li>
-                If you are donating from abroad, please look for fundraisers
-                accepting foreign donations, marked with "£ $ €"
-              </li>
-              <li>
-                If you are an NRI donating from an Indian bank account, your
-                donations don't count as "foreign" and you don't have to worry
-                about this
-              </li>
-              <li>
-                If you have a campaign you want us to add, email
-                covidmutualaidindia@protonmail.com
-              </li>
-            </ul>
-          </p>
-          <p className="italic text-gray-600 my-2">
-            Disclaimer: this list is curated by an informal group of volunteers.
-            Nothing on this site should be taken as a warantee or legal advice.
-            Give at your own discretion, but please do give generously.
-          </p>
+      {!isAlertClosed && (
+        <div className="container pt-6">
+          <div className="relative flex flex-wrap bg-yellow-200 border border-yellow-400 rounded-md py-5 px-8 max-w-3xl mx-auto">
+            <button
+              className="absolute top-0 right-0 p-3"
+              onClick={() => {
+                setIsAlertClosed(true)
+              }}
+              area-pressed="false"
+            >
+              ✕
+            </button>
+            <p className="mt-2 mb-1 font-bold">Info for donors/supporters:</p>
+            <p className="mb-2">
+              <ul className="list-disc pl-5">
+                <li>
+                  Please filter for urgent needs first, marked in red as
+                  "URGENT"
+                </li>
+                <li>
+                  If you are donating from abroad, please look for fundraisers
+                  accepting foreign donations, marked with "£ $ €"
+                </li>
+                <li>
+                  If you are an NRI donating from an Indian bank account, your
+                  donations don't count as "foreign" and you don't have to worry
+                  about this
+                </li>
+                <li>
+                  If you have a campaign you want us to add, email
+                  covidmutualaidindia@protonmail.com
+                </li>
+              </ul>
+            </p>
+            <p className="italic text-gray-600 my-2">
+              Disclaimer: this list is curated by an informal group of
+              volunteers. Nothing on this site should be taken as a warantee or
+              legal advice. Give at your own discretion, but please do give
+              generously.
+            </p>
+          </div>
         </div>
-      </div>}
+      )}
 
       {Object.keys(categories).map((category_header, index) => (
         <div
@@ -134,7 +168,10 @@ export default ({ data }) => {
             {categories[category_header]}
           </h2>
 
-          <Cards nodes={category_cards[categories[category_header]]} isForeignDonor={isForeignDonor} />
+          <Cards
+            nodes={category_cards[categories[category_header]]}
+            isForeignDonor={isForeignDonor}
+          />
         </div>
       ))}
     </Layout>
